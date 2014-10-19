80988a17df90f1d6de2f89cfa0115465b2497ddf
Akshay Mishra <lck.akshay@gmail.com>
2014-10-18 22:50:59 -0400

    Adding telnet client and parser

---
+++ ts-chat-client/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ts-chat-client
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ ts-chat-client/src/ts_chat_client/core.clj
@@ -0,0 +1,64 @@
+(ns ts_chat_client.core
+  (:require [instaparse.core :as insta])
+  (:import (java.net Socket)
+           (java.io PrintWriter InputStreamReader BufferedReader))
+  (:gen-class))
+
+(def fuspr {:name "localhost" :port 25639})
+
+(declare conn-handler)
+
+(def msg-parse  (insta/parser (clojure.java.io/resource "msgparse.bnf")))
+
+(defn connect [server]
+  (let [socket (Socket. (:name server) (:port server))
+        in (BufferedReader. (InputStreamReader. (.getInputStream socket)))
+        out (PrintWriter. (.getOutputStream socket))
+        conn (ref {:in in :out out})]
+    (doto (Thread. #(conn-handler conn)) (.start))
+    conn))
+
+(defn parsed-to-hash [parsed]
+  (let [processed-parsed (map rest parsed)]
+    (conj (reduce #(assoc %1 (first %2) (last %2))
+                  {}
+                  (map #(map last %) (rest processed-parsed)))
+          {"command" (first (first processed-parsed))})))
+
+(defn write [conn msg]
+  (doto (:out @conn)
+    (.println (str msg "\r"))
+    (.flush)))
+
+(defn handle-msg [msg]
+  (cond (or
+         (= msg "")
+         (= "TS3 Client" msg)
+         (= msg "Welcome to the TeamSpeak 3 ClientQuery interface, type
+            \"help\" for a list of commands and \"help <command>\" for
+            information on a specific command.")) nil
+            :else (println msg)))
+
+
+(defn conn-handler [conn]
+  (while (nil? (:exit @conn))
+    (let [msg (.readLine (:in @conn))]
+      (handle-msg msg)
+      (cond
+       (re-find #"^ERROR :Closing Link:=" msg)
+       (dosync (alter conn merge {:exit true}))
+       (re-find #"^PING" msg)
+       (write conn (str "PONG "  (re-find #":.*" msg)))))))
+
+(defn byte-seq [rdr]
+  "create a lazy seq of bytes in a file and close the file at the end"
+  (let [result (. rdr read)]
+    (if (= result -1)
+     (do (. rdr close) nil)
+     (lazy-seq (cons result (byte-seq rdr))))))
+
+(defn -main
+  [& args]
+  (def tn (connect fuspr))
+  (write tn "clientnotifyregister schandlerid=0 event=any"))
+
