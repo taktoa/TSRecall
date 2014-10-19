dbd8ca064e2eba9837338486f60c605c1a570ea2
Akshay Mishra <lck.akshay@gmail.com>
2014-10-19 10:59:25 -0400

    Added ability for messages to be loaded into datomic

--- ts-chat-client/src/ts_chat_client/core.clj
+++ ts-chat-client/src/ts_chat_client/core.clj
@@ -1,57 +1,92 @@
 (ns ts-chat-client.core
   (:require [instaparse.core :as insta])
+  (:use [datomic.api :only [db q] :as d]
+        [ts-chat-client.telnet :as telnet]
+        )
   (:import (java.net Socket)
            (java.io PrintWriter InputStreamReader BufferedReader))
   (:gen-class))
 
-(def fuspr {:name "localhost" :port 25639})
+(def uri
+  ;"datomic:free://localhost:4334")
+  "datomic:mem://127.0.0.1:4334")
 
-(declare conn-handler)
+(defn make-db []
+  (d/create-database uri))
 
-(def msg-parse  (insta/parser (clojure.java.io/resource "msgparse.bnf")))
+(make-db)
 
-(defn connect [server]
-  (let [socket (Socket. (:name server) (:port server))
-        in (BufferedReader. (InputStreamReader. (.getInputStream socket)))
-        out (PrintWriter. (.getOutputStream socket))
-        conn (ref {:in in :out out})]
-    (doto (Thread. #(conn-handler conn)) (.start))
-    conn))
+(defn delete-db []
+  (d/delete-database uri))
 
-(defn parsed-to-hash [parsed]
-  (let [processed-parsed (map rest parsed)]
-    (conj (reduce #(assoc %1 (first %2) (last %2))
-                  {}
-                  (map #(map last %) (rest processed-parsed)))
-          {"command" (first (first processed-parsed))})))
+(def conn (d/connect uri))
 
-(defn write [conn msg]
-  (doto (:out @conn)
-    (.println (str msg "\r"))
-    (.flush)))
+(defn add-person-attribute []
+  (d/transact conn [{:db/id #db/id[:db.part/db]
+                     :db/ident :person/name
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one
+                     :db/doc "A person's name"
+                     :db.install/_attribute :db.part/db}]))
 
-(defn handle-msg [msg]
-  (println msg))
 
-(defn conn-handler [conn]
-  (while (nil? (:exit @conn))
-    (let [msg (.readLine (:in @conn))]
-      (handle-msg msg)
-      (cond
-       (re-find #"^ERROR :Closing Link:=" msg)
-       (dosync (alter conn merge {:exit true}))
-       (re-find #"^PING" msg)
-       (write conn (str "PONG "  (re-find #":.*" msg)))))))
+(defn add-address-attribute []
+  (d/transact conn [{:db/id #db/id[:db.part/db]
+                     :db/ident :person/address
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one
+                     :db/doc "A person's address"
+                     :db.install/_attribute :db.part/db}]))
 
-(defn byte-seq [rdr]
-  "create a lazy seq of bytes in a file and close the file at the end"
-  (let [result (. rdr read)]
-    (if (= result -1)
-     (do (. rdr close) nil)
-     (lazy-seq (cons result (byte-seq rdr))))))
+(defn add-chat-attribute []
+  (d/transact conn [{:db/id #db/id[:db.part/db]
+                     :db/ident :msg/text
+                     :db/valueType :db.type/string
+                     :db/cardinality :db.cardinality/one
+                     :db/doc "a chat's log"
+                     :db.install/_attribute :db.part/db}]))
+
+
+(add-person-attribute)
+(add-address-attribute)
+(add-chat-attribute)
+
+
+(defn add-a-person [name]
+  (d/transact conn [{:db/id #db/id[:db.part/user] :person/name name}]))
+
+(defn add-mesg [message]
+  (d/transact conn [{:db/id #db/id[:db.part/user] :msg/text message}]))
+
+(defn get-all-people []
+  (q '[:find ?n :where [?c person/name ?n ]] (db conn)))
+
+(defn get-all-chats []
+  (q '[:find ?n :where [?c msg/text ?n ]] (db conn)))
+
+(defn find-person [name dbc]
+  (first (first (q `[:find ?c :where [?c person/name ~name]] dbc))))
+
+(defn find-address [address dbc]
+  (first (first (q `[:find ?c :where [?c person/name ~name]] dbc))))
+
+(defn illustrators-that-live-with [name]
+  (q `[:find ?n1 :where
+       [?c person/name ~name]
+       [?c person/address ?a]
+       [?n person/name ?n1]]  (db conn)))
+
+(defn add-address [name address]
+  (let [dbc (db conn)
+        id (find-person name dbc)]
+    (d/transact conn [{:db/id id :person/msg address}])))
+
+(defn shit [m]
+  (if (= "notifytalkstatuschange" (get m "command")) (add-mesg (str (get m "invokername") (get m "msg"))) nil))
 
 (defn -main
   [& args]
-  (def tn (connect fuspr))
-  (write tn "clientnotifyregister schandlerid=0 event=any"))
+   (def tn (telnet/connect fuspr shit))
+   (telnet/write tn "clientnotifyregister schandlerid=0 event=any")
+  )
 
